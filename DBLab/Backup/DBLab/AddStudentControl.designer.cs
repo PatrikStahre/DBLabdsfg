58beb548a8fa524ca66588929d69d10d06520a46
PatrikStahre <63604248+PatrikStahre@users.noreply.github.com>
2020-04-20 15:17:57 +0200

    Add files via upload

---
+++ DBLab/Backup/DBLab/AddStudentControl.designer.cs
@@ -0,0 +1,59 @@
+﻿namespace DBLabs
+{
+    partial class AddStudentControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddStudentGB = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // AddStudentGB
+            // 
+            this.AddStudentGB.Location = new System.Drawing.Point(14, 12);
+            this.AddStudentGB.Name = "AddStudentGB";
+            this.AddStudentGB.Size = new System.Drawing.Size(1051, 507);
+            this.AddStudentGB.TabIndex = 54;
+            this.AddStudentGB.TabStop = false;
+            this.AddStudentGB.Text = "Add Student";
+            // 
+            // AddStudentControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.AddStudentGB);
+            this.Name = "AddStudentControl";
+            this.Size = new System.Drawing.Size(1087, 533);
+            this.Load += new System.EventHandler(this.LoadAddStudentControl);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox AddStudentGB;
+    }
+}
