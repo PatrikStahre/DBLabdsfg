cb932ccfb58e21af2af49ac134954ab1735e74c0
all16002 <50872523+all16002@users.noreply.github.com>
2020-04-22 17:17:53 +0200

    Testar här

---
+++ DBLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DBLabs
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        [STAThread]
+        static void Main()
+        {
+            DBConnection dbconn = new DBConnection();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ApplicationForm(ref dbconn));
+        }
+    }
+}
